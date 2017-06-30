4bffae037aaae2c71f3c3bbe6dd4702f43747af7
Nandha kumar S <mailstonandhakumar@gmail.com>
2017-06-30 10:53:00 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+void main()
+{
+int a,b;
+printf("\nEnter the number:");
+scanf("\n%d",&a);
+if(a>0)
+{
+b=a*a;
+printf("\npower of the num is %d",b);
+}
+else
+printf("\nEnter numeric values");
+getch();
+}
